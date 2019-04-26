7db63262ce74b461b1ac7cc1db48a57f44c078aa
shunf4 <shun1048576@gmail.com>
2019-04-26 15:19:13 +0800

    Kmem_cache malloc implementation [fix]

--- UNIXV6PP/CCUtils.cc
+++ UNIXV6PP/CCUtils.cc
@@ -3,7 +3,7 @@
 int __gxx_personality_v0;
 int _Unwind_Resume;
 
-#if false
+
 void *operator new(unsigned long x) {
 	return(secondfs_c_helper_malloc(x));
 }
@@ -12,6 +12,7 @@ void operator delete(void *pointer) {
 	secondfs_c_helper_free(pointer);
 }
 
+#if false
 void operator delete(void *pointer, const long unsigned int type) {
 	secondfs_c_helper_free(pointer);
 }
