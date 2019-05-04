f1bcbaca4211758f39fbf8aed7e5e87d61f0d4f9
shunf4 <shun1048576@gmail.com>
2019-05-04 14:37:37 +0800

    Try to fix compile & link error

--- UNIXV6PP/Inode_c_wrapper.h
+++ UNIXV6PP/Inode_c_wrapper.h
@@ -57,7 +57,7 @@ typedef struct Inode
 	s32		i_count;		/* 引用计数 */
 	s32		i_nlink;		/* 文件联结计数，即该文件在目录树中不同路径名的数量 */
 	
-	_SuperBlock*	i_ssb;			/* 外存inode所在 SuperBlock */
+	struct _SuperBlock*	i_ssb;			/* 外存inode所在 SuperBlock */
 	s32		i_number;		/* 外存inode区中的编号 */
 	
 	u16		i_uid;			/* 文件所有者的用户标识数 */
