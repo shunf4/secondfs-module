55fb9f27318d7792610fbbb5d009c8e7536335a1
shunf4 <shun1048576@gmail.com>
2019-05-19 22:51:32 +0800

    Make code tidy completed [fix]

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo insmod secondfs.ko
-echo ====== | sudo dd of=/dev/kmsg > /dev/null 2>&1
-sudo rmmod secondfs.ko
-dmesg | tail -60
+#echo ====== | sudo dd of=/dev/kmsg > /dev/null 2>&1
+#sudo rmmod secondfs.ko
+#dmesg | tail -60
+sudo mount -t secondfs -o loop c.img ./test
