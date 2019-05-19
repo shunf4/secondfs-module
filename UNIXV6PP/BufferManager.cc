c790fd447e5f3170b8bd3101e546fa026149bb95
shunf4 <shun1048576@gmail.com>
2019-05-19 21:26:08 +0800

    Make code tidy completed [fix]

--- UNIXV6PP/BufferManager.cc
+++ UNIXV6PP/BufferManager.cc
@@ -235,8 +235,8 @@ loop:
 	return bp;
 }
 
-extern "C" void BufferManager_Brelse(BufferManager *bm, Buf* bp) { bm->Brelse(bp); }
-void BufferManager::Brelse(Buf* bp)
+extern "C" void BufferManager_Brelse(BufferManager *bm, Buf* bp) { bm->Brelse(bp, 1); }
+void BufferManager::Brelse(Buf* bp, int print = 1)
 {
 	/* 临界资源，比如：在同步读末期会调用这个函数，
 	 * 此时很有可能会产生磁盘中断，同样会调用这个函数。
