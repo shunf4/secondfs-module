a7ac0ac2200d3475232ea12f4af38d72472785d6
shunf4 <shun1048576@gmail.com>
2019-06-03 20:54:22 +0800

    Add mkfs [FIX]

--- _test.sh
+++ _test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo insmod secondfs.ko
-#echo ====== | sudo dd of=/dev/kmsg > /dev/null 2>&1
-#sudo rmmod secondfs.ko
-#dmesg | tail -60
+mkdir -p test test2
+sudo mount -t secondfs -o loop new.img ./test2
 sudo mount -t secondfs -o loop c.img ./test
